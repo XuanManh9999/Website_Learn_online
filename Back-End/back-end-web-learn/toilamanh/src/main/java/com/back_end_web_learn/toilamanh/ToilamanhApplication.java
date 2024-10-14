42f3b289338d926530559d73b8ef07456bf595e7
XuanManh9999 <nguyenxuanmanh030929@gmail.com>
2024-10-14 16:02:34 +0700

    Init Project

---
+++ Back-End/back-end-web-learn/toilamanh/src/main/java/com/back_end_web_learn/toilamanh/ToilamanhApplication.java
@@ -0,0 +1,13 @@
+package com.back_end_web_learn.toilamanh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToilamanhApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ToilamanhApplication.class, args);
+	}
+
+}
---
+++ Back-End/back-end-web-learn/toilamanh/src/test/java/com/back_end_web_learn/toilamanh/ToilamanhApplicationTests.java
@@ -0,0 +1,13 @@
+package com.back_end_web_learn.toilamanh;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToilamanhApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
