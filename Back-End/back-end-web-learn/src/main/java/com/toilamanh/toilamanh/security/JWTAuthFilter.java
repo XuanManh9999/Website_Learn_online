2c7d9dff5e2884ab8c192477bae481e80819beb5
XuanManh9999 <nguyenxuanmanh030929@gmail.com>
2024-10-15 17:17:18 +0700

    Add security config

---
+++ Back-End/back-end-web-learn/src/main/java/com/toilamanh/toilamanh/security/JWTAuthFilter.java
@@ -0,0 +1,52 @@
+package com.toilamanh.toilamanh.security;
+
+import com.toilamanh.toilamanh.service.CustomUserDetailsService;
+import com.toilamanh.toilamanh.utils.JWTUtils;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@Component
+public class JWTAuthFilter extends OncePerRequestFilter{
+    private final JWTUtils jwtUtils;
+    @Autowired
+    private CustomUserDetailsService customUserDetailsService;
+    public JWTAuthFilter(JWTUtils jwtUtils) {
+        this.jwtUtils = jwtUtils;
+    }
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException, IOException {
+        final String authorizationHeader = request.getHeader("Authorization");
+        final String jwtToken;
+        final String userEmail;
+        if (authorizationHeader == null || !authorizationHeader.startsWith("Bearer ") || authorizationHeader.isBlank()) {
+            filterChain.doFilter(request, response);
+            return;
+        }
+        jwtToken = authorizationHeader.substring(7);
+        userEmail = jwtUtils.extractUsername(jwtToken);
+        if (userEmail != null && SecurityContextHolder.getContext().getAuthentication() == null) {
+            UserDetails userDetails = customUserDetailsService.loadUserByUsername(userEmail);
+            if (jwtUtils.isValidToken(jwtToken, userDetails)) {
+                SecurityContext securityContext = SecurityContextHolder.createEmptyContext();
+                UsernamePasswordAuthenticationToken authentication = new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
+                authentication.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+                securityContext.setAuthentication(authentication);
+                SecurityContextHolder.setContext(securityContext);
+            }
+        }
+        filterChain.doFilter(request, response);
+    }
+}
